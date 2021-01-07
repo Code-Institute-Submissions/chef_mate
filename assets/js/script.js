7ec0165ad5774aaf530c54aa69e2a27806794a99
evendoom <davidej.p.correia@gmail.com>
2021-01-07 22:38:10 +0000

    feat: Add responsive navigation and more media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
+/* JQuery Sticky Navigation*/
+
 $(document).ready(function() {
 
-    $('.section-features-sticky').waypoint(function(direction) {
+    $('.js--section-features').waypoint(function(direction) {
         if (direction == 'down') {
+            $('nav').removeClass('nav-container')
             $('nav').addClass('sticky-nav');
         }
 
         else {
+            $('nav').addClass('nav-container');
             $('nav').removeClass('sticky-nav');
         }
     }, {
-        offset: '20%'
-    })
+        offset: '15%'
+    });
+
 });
+
+function menuFunction() {
+    document.getElementById('nav-links-container').classList.toggle('js--nav-links-container-show');
+    document.getElementById('js--menu-icon').classList.toggle('js--icon-display-none');
+    document.getElementById('js--menu-icon-close').classList.toggle('js--icon-display-block');
+}
